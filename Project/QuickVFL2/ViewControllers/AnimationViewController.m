7c5760b949680aa3803f7cac2805041a79b67af5
Sody <longtem@gmail.com>
2017-12-06 10:33:38 +0800

    update the animation mode.

--- Project/QuickVFL2/ViewControllers/AnimationViewController.m
+++ Project/QuickVFL2/ViewControllers/AnimationViewController.m
@@ -12,7 +12,6 @@
 @property (nonatomic, weak) UIView* imageWrapper;
 @property (nonatomic, weak) UIScrollView* scrollView;
 @property (nonatomic, weak) UIImageView* imageView;
-@property (nonatomic, weak) NSLayoutConstraint* constraintPickerControl;
 @property (nonatomic, weak) UIDatePicker* datePicker_;
 @property (nonatomic, weak) UIView* viewMask;
 @end
@@ -37,11 +36,11 @@
 -(void)controlPickerVisibity:(BOOL)visible{
     [UIView animateWithDuration:0.4 animations:^{
         if(visible){
-            self.constraintPickerControl.constant = -1 * self.datePicker_.superview.frame.size.height;
+            self.datePicker_.superview.transform = CGAffineTransformMakeTranslation(0, -1 * self.datePicker_.superview.frame.size.height);
             self.viewMask.alpha = 1;
             self.viewMask.userInteractionEnabled = YES;
         } else {
-            self.constraintPickerControl.constant = 0;
+            self.datePicker_.superview.transform = CGAffineTransformIdentity;
             self.viewMask.alpha = 0;
             self.viewMask.userInteractionEnabled = NO;
         }
@@ -54,7 +53,6 @@
     BOOL hidden = [self.imageWrapper q_visibleVertically:YES];
     [UIView animateWithDuration:0.6 animations:^{
         [self.imageWrapper q_setVisibility:!hidden isVertically:YES];
-        
         [self.view layoutIfNeeded];
     } completion:^(BOOL finished) {
         [self.scrollView q_refreshContentView];
